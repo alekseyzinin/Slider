281ebd382b71bdf483e10af093baa188a351e17a
Aleksejzinin <aleksejzinin@yandex.ru>
2021-02-01 20:41:47 +0400

    fixed bugs in the code js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const prev = document.getElementsById('btn-prev'),
-      next = document.getElementsById('btn-next'),
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
       slides = document.querySelectorAll('.slide'),
       dots = document.querySelectorAll('.dot');
 
