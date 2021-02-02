412c64da9a939dcfaf1a7be288de7b422212551b
Aleksejzinin <aleksejzinin@yandex.ru>
2021-02-03 00:23:33 +0400

    added active points and interval

--- script.js
+++ script.js
@@ -12,25 +12,46 @@ const activeSlide = n => {
     slides[n].classList.add('active');
 }
 
+const activeDot = n => {
+    for(dot of dots) {
+       dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+
 const nextSlide = () => {
     if(index == slides.length - 1) {
         index = 0;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     } else {
         index++;
-        activeSlide(index);
+        prepareCurrentSlide(index)
     }
 }
 
 const prevSlide = () => {
     if(index == 0) {
         index = slides.length - 1
-        activeSlide(index);
+        prepareCurrentSlide(index);
     } else {
         index--;
-        activeSlide(index);
+        prepareCurrentSlide(index);
     }
 }
 
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+})
+
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+
+setInterval(nextSlide, 3000);
